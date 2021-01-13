c1474c8eb6c891038d415ee443f439d151bcf2e8
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-13 11:59:46 -0500

    initial commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def clean?
+        return @hygiene > 7 ? true : false        
+    end
+    
+
+end
